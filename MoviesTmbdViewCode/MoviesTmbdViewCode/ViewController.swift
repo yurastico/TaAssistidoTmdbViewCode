05154bbcfdba9d5e5c59ca579abf22ad0a30c4b3
yurastico <cunha.ysc@gmail.com>
2024-01-07 21:50:44 -0300

    setting up project

---
+++ MoviesTmbdViewCode/MoviesTmbdViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesTmbdViewCode
+//
+//  Created by Yuri Cunha on 07/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
