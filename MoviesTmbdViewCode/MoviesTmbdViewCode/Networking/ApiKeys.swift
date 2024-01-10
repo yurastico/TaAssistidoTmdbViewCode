56098ba51c6efbeccd34d806d8de14d541702ee7
Yuri cunha <40001355+yurastico@users.noreply.github.com>
2024-01-10 09:08:06 -0300

    Update ApiKeys.swift

--- MoviesTmbdViewCode/MoviesTmbdViewCode/Networking/ApiKeys.swift
+++ MoviesTmbdViewCode/MoviesTmbdViewCode/Networking/ApiKeys.swift
@@ -7,7 +7,3 @@
 
 import Foundation
 
-enum ApiKeys: String {
-    case key = "2e356f6365bdb38ef602b4e1cc2d9afd"
-    case token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTM1NmY2MzY1YmRiMzhlZjYwMmI0ZTFjYzJkOWFmZCIsInN1YiI6IjY1OWQ5ODMyMWQzNTYzMDA5NWQ2YTZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P-XHQxkbsZGcw-HA_k8pgM5z0mEG9-4vi9hx5p_zd3c"
-}
